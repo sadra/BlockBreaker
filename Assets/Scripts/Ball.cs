632c4e08d648cba92073a2ba8e6fd122534a3fca
sadra amlashi <amlashi.sadra@gmail.com>
2018-12-22 10:21:41 +0330

    init master

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    //Config pramateres
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float randomFactor = 0.5f;
+
+    //state
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
+    //Cached component refrence
+    AudioSource myAudioSource;
+    Rigidbody2D myRigidbody2D;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted) {
+            LockBallToPaddle();
+            LunchOnMouseClick();
+        }
+    }
+
+    private void LunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            myRigidbody2D.velocity = new Vector2(xPush, yPush);
+            hasStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        Vector2 velocityTweak = new Vector2(Random.Range(-randomFactor, randomFactor), Random.Range(-randomFactor, randomFactor));
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+            myRigidbody2D.velocity += velocityTweak;
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameSession : MonoBehaviour
+{
+    // Configurations parameters
+    [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsBlockDestroyed = 1;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] bool isAutoPlayEnabled;
+    [SerializeField] TextMeshProUGUI levelText;
+
+    // State Variables
+    [SerializeField] int currentScore = 0;
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameSession>().Length;
+        if (gameStatusCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+        levelText.text = SceneManager.GetActiveScene().name;
+    }
+
+    public void AddToScore() 
+    {
+        currentScore += pointsBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+
+    public bool IsAutoPlayEnabled()
+    {
+        return isAutoPlayEnabled;
+    }
+
+}
